6e1dc83554c40f797e783a9785cf00c7b1118644
4skinSkywalker <stopchemtrailsfred@gmail.com>
2021-12-07 16:48:26 +0100

    Optimize images and preload them

--- js/main.js
+++ js/main.js
@@ -1,137 +1,155 @@
-// TODO: optimize images and listen for onload (maybe do it with a promise)
-function preloadImages(imageUrls) {
-    let images = [];
-    for (let imgUrl of imageUrls) {
-        let img = new Image();
-        img.src = imgUrl;
-        images.push(img);
-    }
-    return images;
-}
-
-function startEboxLogoAnimation() {
-    document.querySelector('#ebox-logo-animation-image').setAttribute('src','img/ebox-logo-animation.gif');
-}
-startEboxLogoAnimation();
-
-// define images
-var images = Array(20).fill(0).map((_, i) =>
-    `img/ebox-logo-animation-frames/frame_${ (i+"").padStart(2, "0") }_delay-0.04s.png`
-);
-// preloadImages(images);
-
-// TweenMax can tween any property of any object. We use this object to cycle through the array
-var obj = {curImg: 0};
-
-// create tween
-var tween = TweenMax.to(obj, 0.5,
-    {
-        curImg: images.length - 1,	// animate propery curImg to number of images
-        roundProps: "curImg",		// only integers so it can be used as an array index
-        repeat: 3,					// repeat 3 times
-        immediateRender: false,	    // load first image automatically
-        ease: Linear.easeNone,		// show every image the same ammount of time
-        onUpdate: function () {
-          document.querySelector("#ebox-logo-animation-image").setAttribute("src", images[obj.curImg]);                 // set the image source
-        }
-    }
+// Vars
+var eboxLogoImageUrls = Array(20).fill(0).map((_, i) =>
+    `img/ebox-logo-animation-frames/frame_${(i + "").padStart(2, "0")}_delay-0.04s-min.png`
 );
 
-// init controller
-var controller = new ScrollMagic.Controller();
-
-// build scene
-var scene = new ScrollMagic.Scene({ triggerElement: "#ebox-logo-animation-trigger", duration: 300 })
-                           .setTween(tween)
-                           .addIndicators() // add indicators (requires plugin)
-                           .addTo(controller);
-
-
-
-
+var rotatingCubeImageUrls = Array(61).fill(0).map((_, i) =>
+    `img/rotating-cube/frame_${(i + "").padStart(2, "0")}_delay-0.03s-min.png`
+);
 
 var textForRotation = ["Made Simple", "Made Safe", "Made Reversible", "Reinvented"];
 var textForRotationIndex = 1;
 var rotateTime = 3000;
-function rotateText() {
-	document.querySelector("#rotatedText").style.opacity = "0";
 
-	setTimeout(function() {
-		document.querySelector("#rotatedText").innerHTML = textForRotation[textForRotationIndex];
-		document.querySelector("#rotatedText").style.opacity = "1";
+let loader = new SVGLoader(
+    document.getElementById('loader'),
+    { speedIn: 300, easingIn: mina.easeinout }
+);
+
+// Functions
+function rotateText() {
+    document.querySelector("#rotatedText").style.opacity = "0";
+
+    setTimeout(function() {
+        document.querySelector("#rotatedText").innerHTML = textForRotation[textForRotationIndex];
+        document.querySelector("#rotatedText").style.opacity = "1";
 
         document.querySelector("#rotatedTextUnderline").style.width = document.querySelector("#rotatedText").offsetWidth + "px";
-		
-		textForRotationIndex = ++textForRotationIndex % textForRotation.length;
-	
-		setTimeout(rotateText, rotateTime);
-	}, 450);
+        
+        textForRotationIndex = ++textForRotationIndex % textForRotation.length;
+    
+        setTimeout(rotateText, rotateTime);
+    }, 450);
 }
-rotateText();
 
+function preloadImg(imgUrl) {
+    return new Promise((resolve) => {
+        let img = new Image();
+        img.onload = () => resolve(img);
+        img.src = imgUrl;
+    });
+}
 
+function preloadImgs(imageUrls) {
+    return Promise.all(
+        imageUrls.map(imageUrl => preloadImg(imageUrl))
+    );
+}
 
+function replaceImg(oldImg, newImg) {
+    let parent = oldImg.parentElement;
+    newImg.id = oldImg.id;
+    newImg.alt = oldImg.alt;
+    parent.replaceChild(newImg, oldImg);
+}
 
-// define images
-var images2 = Array(61).fill(0).map((_, i) =>
-    `img/rotating-cube/frame_${ (i+"").padStart(2, "0") }_delay-0.03s.png`
-);
-// preloadImages(images2);
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(() => resolve(true), ms));
+}
 
-// TweenMax can tween any property of any object. We use this object to cycle through the array
-var obj2 = {curImg: 0};
+function scrollParallax(elementSelector, triggerElement, duration, transformFrom, transformTo) {
 
-// create tween
-var tween = TweenMax.to(obj2, 0.5,
-    {
-        curImg: images2.length - 1,	// animate propery curImg to number of images
-        roundProps: "curImg",		// only integers so it can be used as an array index
-        repeat: 3,					// repeat 3 times
-        immediateRender: true,	    // load first image automatically
-        ease: Linear.easeNone,		// show every image the same ammount of time
-        onUpdate: function () {
-          document.querySelector("#rotating-cube").setAttribute("src", images2[obj2.curImg]);                         // set the image source
+    var tween = new TimelineMax().
+        add([
+            TweenMax.fromTo(
+                elementSelector,
+                1,
+                { transform: transformFrom },
+                { transform: transformTo, ease: Linear.easeNone }
+            )
+        ]);
+
+    // build scene
+    new ScrollMagic.Scene({ triggerElement, duration }).
+        setTween(tween).
+        addTo(new ScrollMagic.Controller());
+}
+
+function scrollImgSequencer(images, imageSelector, triggerElement, duration) {
+
+    // TweenMax can tween any property of any object
+    let obj = { curImg: 0 };
+
+    // create tween
+    let tween = TweenMax.to(obj, 0.5,
+        {
+            curImg: images.length - 1,	// animate propery curImg to number of images
+            roundProps: "curImg",		// only integers so it can be used as an array index
+            repeat: 0,					// repeat 0 times
+            immediateRender: false,	    // load first image automatically
+            ease: Linear.easeNone,		// show every image the same ammount of time
+            onUpdate: function () {
+                replaceImg(
+                    document.querySelector(imageSelector),
+                    images[obj.curImg]
+                );
+            }
         }
-    }
-);
+    );
 
-// init controller
-var controller = new ScrollMagic.Controller();
+    // build scene
+    new ScrollMagic.
+        Scene({ triggerElement, duration }).
+        setTween(tween).
+        addIndicators().
+        addTo(new ScrollMagic.Controller());
+}
 
-// build scene
-var scene = new ScrollMagic.Scene({ triggerElement: "#rotating-cube-trigger", duration: 555 })
-                           .setTween(tween)
-                        //    .addIndicators() // add indicators (requires plugin)
-                           .addTo(controller);
+// Async execution context
+(async function() {
 
+    loader.show();
 
+    // Preloading images (may take a while)
+    let eboxLogoImages = await preloadImgs(eboxLogoImageUrls);
+    let rotatingCubeImages = await preloadImgs(rotatingCubeImageUrls);
+    let preloadedEboxLogo = await preloadImg("img/ebox-logo-animation.gif");
 
+    await delay(450);
 
+    loader.hide();
 
-// build tween
-var tween = new TimelineMax ()
-.add([
-    TweenMax.fromTo("#rotating-cube", 1, { transform: "translateY(150px)" }, { transform: "translateY(-150px)", ease: Linear.easeNone })
-]);
+    replaceImg(
+        document.querySelector("#ebox-logo-animation-image"),
+        preloadedEboxLogo
+    );
+    
+    let site = document.getElementById('site');
+    classie.removeClass( site, 'show' );
+    classie.addClass( site, 'show' );
 
-// build scene
-var scene = new ScrollMagic.Scene({ triggerElement: "#cube-parallax-trigger", duration: 666 })
-                           .setTween(tween)
-                        //    .addIndicators() // add indicators (requires plugin)
-                           .addTo(controller);
+    rotateText();
 
+    scrollImgSequencer(
+        eboxLogoImages,
+        "#ebox-logo-animation-image",
+        "#ebox-logo-animation-trigger",
+        333
+    );
 
+    scrollImgSequencer(
+        rotatingCubeImages,
+        "#rotating-cube",
+        "#rotating-cube-trigger",
+        555
+    );
+    
+    scrollParallax(
+        "#magic-border-1 .magic-border-inner",
+        "#magic-border-1-trigger",
+        333,
+        "translate(0, 0)",
+        "translate(-2rem, -2rem)"
+    );
 
-
-
-// build tween
-var tween = new TimelineMax ()
-.add([
-    TweenMax.fromTo("#magic-border-1 .magic-border-inner", 1, { transform: "translate(0, 0)" }, { transform: "translate(-2rem, -2rem)", ease: Linear.easeNone })
-]);
-
-// build scene
-var scene = new ScrollMagic.Scene({ triggerElement: "#magic-border-1-trigger", duration: 333 })
-                           .setTween(tween)
-                        //    .addIndicators() // add indicators (requires plugin)
-                           .addTo(controller);
+})();
